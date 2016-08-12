60a19ae62b815575b510a946e3bbd5345d212333
Kris Zyp <kriszyp@gmail.com>
2016-08-12 12:58:46 -0600

    Allow generators to be bound

--- test/react.js
+++ test/react.js
@@ -1,7 +1,33 @@
-let num = react(3)
-let sum = react(num + 5)
-let bool = react(true)
-let f = react(!bool)
-let cond = react(f ? num : sum)
-let result
-react(result = Math.min(num, sum))
+var tests = {
+  basic: function() {
+    let num = react(3)
+  },
+  sum: function() {
+    let sum = react(num + 5)
+  },
+  bool: function() {
+    let bool = react(true)
+    let f = react(!bool)
+  },
+  cond: function() {
+    let cond = react(f ? num : sum)
+  },
+  call: function() {
+    let result
+    react(result = Math.min(num, sum))
+  },
+  boundGenerator: function() {
+    react((function*() {
+      test()
+    }).bind(this))
+  }
+}
+var test
+for (var testName in tests) {
+  var result = require("babel-core").transform('test=' + tests[testName].toString(), {
+    plugins: ["transform-alkali"]
+  })
+  console.log('transformed', result.code)
+  //eval(result.code)()
+}
+
